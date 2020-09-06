f52d28c16b02d748f16608f22566746e251ddd0d
SethKeatley <SethKeatley@users.noreply.github.com>
2020-09-06 11:23:40 -0400

    Landing page created, working on getting number of letters for text boxes to work.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class JumbleSolver
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNumLetters = new System.Windows.Forms.TextBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.pnlLetters = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // txtNumLetters
+            // 
+            this.txtNumLetters.AcceptsReturn = true;
+            this.txtNumLetters.Location = new System.Drawing.Point(277, 175);
+            this.txtNumLetters.Name = "txtNumLetters";
+            this.txtNumLetters.Size = new System.Drawing.Size(151, 26);
+            this.txtNumLetters.TabIndex = 0;
+            this.txtNumLetters.Text = "Number of Letters";
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(309, 249);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(82, 34);
+            this.btnConfirm.TabIndex = 1;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // pnlLetters
+            // 
+            this.pnlLetters.Location = new System.Drawing.Point(156, 113);
+            this.pnlLetters.Name = "pnlLetters";
+            this.pnlLetters.Size = new System.Drawing.Size(390, 130);
+            this.pnlLetters.TabIndex = 2;
+            // 
+            // JumbleSolver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pnlLetters);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.txtNumLetters);
+            this.Name = "JumbleSolver";
+            this.Text = "Jumble Solver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNumLetters;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Panel pnlLetters;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,11 +10,34 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class JumbleSolver : Form
     {
-        public Form1()
+        public int numLetters;
+
+        private int txtBoxStartPosition = 100;
+        private int txtBoxStartPositionV = 25;
+        public JumbleSolver()
         {
             InitializeComponent();
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            numLetters = int.Parse(txtNumLetters.Text);
+            txtNumLetters.Hide();
+            btnConfirm.Hide();
+
+
+            for(int i = 0; i <= numLetters; i++)
+            {
+                TextBox newTxt = new TextBox();
+                newTxt.Location = new System.Drawing.Point(txtBoxStartPosition, txtBoxStartPositionV);
+                newTxt.Size = new System.Drawing.Size(70, 40);
+                newTxt.Text = i.ToString();
+
+                pnlLetters.Add(newTxt);
+                txtBoxStartPositionV += 30;
+            }
         }
     }
 }
